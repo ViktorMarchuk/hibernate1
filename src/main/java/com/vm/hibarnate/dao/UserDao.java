bb4dc19f72a1129521e2621c3dd15bf0ee30632c
Viktor Marchuk <viktor.m77.77@mail.ru>
2024-01-09 23:03:35 +0300

    commit 9/01

--- src/main/java/com/vm/hibarnate/dao/UserDao.java
+++ src/main/java/com/vm/hibarnate/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.vm.hibarnate.dao;
 import com.vm.hibarnate.entity.User;
 import org.hibernate.Session;
 
+import java.sql.Date;
 import java.util.List;
 
 public class UserDao {
@@ -28,6 +29,26 @@ public class UserDao {
         var user = criteria.from(User.class);
         criteria.select(user)
                 .where(criteriaBuild.equal(user.get("personalInfo").get("firstName"), firstName));
+        return session.createQuery(criteria).list();
+    }
+
+    public List<User> findAllByCompanyName(Session session, String company) {
+        var cb = session.getCriteriaBuilder();
+        var criteria = cb.createQuery(User.class);
+        var user = criteria.from(User.class);
+        criteria.select(user)
+                .where(cb.equal(user.get("company").get("name"), company));
+
+        return session.createQuery(criteria).list();
+    }
+
+    public List<User> findUserByBirthday(Session session, Date date) {
+        var cb = session.getCriteriaBuilder();
+        var criteria = cb.createQuery(User.class);
+        var user = criteria.from(User.class);
+        criteria.select(user)
+                .where(cb.greaterThan(user.get("personalInfo").get("birthDay"), date));
+
         return session.createQuery(criteria).list();
     }
 }
